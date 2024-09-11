88e14a054f17c55b3ca3dae49aec40feb0d68014
th92rodr <45070690+th92rodr@users.noreply.github.com>
2024-09-11 19:05:58 -0300

    Server: create get-week-summary route and usecase

---
+++ server/src/http/routes/get-week-summary.ts
@@ -0,0 +1,9 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { getWeekSummary } from '../../usecases/get-week-summary'
+
+export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
+  app.get('/summary', async () => {
+    const { summary } = await getWeekSummary()
+    return { summary }
+  })
+}
